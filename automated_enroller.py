0de271805c4545c6577e0736a5666783b53dcd7e
Egco <egecan2811@yandex.com>
2024-09-18 15:03:07 +0300

    added login

--- automated_enroller.py
+++ automated_enroller.py
@@ -8,3 +8,9 @@ driver = webdriver.Chrome(options=options)
 
 with open("credentials.txt", "r") as file:
     file.read()
+
+driver.get("https://kepler-beta.itu.edu.tr")
+driver.find_element(by=By.NAME, value="ctl00$ContentPlaceHolder1$tbUserName").send_keys(username)
+driver.find_element(by=By.NAME, value="ctl00$ContentPlaceHolder1$tbPassword").send_keys(password)
+driver.find_element(by=By.NAME, value="ctl00$ContentPlaceHolder1$btnLogin").click()
+
