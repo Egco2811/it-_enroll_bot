ea8ed37481bbb8bb43e64417c145314d927ec6b9
Egco <egecan2811@yandex.com>
2024-09-18 15:01:56 +0300

    detached browser

--- automated_enroller.py
+++ automated_enroller.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
 with open("credentials.txt", "r") as file:
     file.read()
